0df2ac07a0a1c8ce2f3770bb23c7eeb84b1b160d
Mark Dexter Abuyabor <a.markdexter@gmail.com>
2024-01-03 21:16:31 +0800

    add: mongo setup

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,28 @@
 const express = require('express');
+const mongoose = require('mongoose');
+
 require('dotenv').config();
 
 const app = express();
 const PORT = process.env.PORT
 
-app.listen(PORT, () => {
-    console.log(`Listening to PORT ${PORT}`)
-})
+// HTTP route
+app.get('/', (req, res) => {
+    console.log(req)
+    return res.status(200).send('Hello World')
+});
+
+// mongo connect
+mongoose
+    .connect(process.env.MONGO_URI)
+    .then(() => {
+        console.log(`DB connected successfully`)
+
+        app.listen(PORT, () => {
+            console.log(`Listening to PORT ${PORT}`)
+        })
+        
+    })
+    .catch((err) => {
+        console.log(`Error: ${err}`)
+    })
