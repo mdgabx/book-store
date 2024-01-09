a5f6a3b3b8f0c2a77052a2e2dddb05cc1497ed07
Mark Dexter Abuyabor <a.markdexter@gmail.com>
2024-01-09 21:00:09 +0800

    feat: back button component

--- frontend/src/pages/ShowBook.jsx
+++ frontend/src/pages/ShowBook.jsx
@@ -1,7 +1,33 @@
+import { useState, useEffect } from "react"
+import axios from 'axios'
+import { useParams } from "react-router-dom"
+import BackButton from "../components/BackButton"
 
 const ShowBook = () => {
+  const [book, setBook] = useState({})
+  const [loading, setLoading] = useState(false)
+  const { id } = useParams();
+
+  useEffect(() => {
+    setLoading(true)
+
+    axios
+      .get(`http://localhost:5151/book/${id}`)
+      .then((res) => {
+        setBook(res.data)
+        setLoading(false)
+      })
+      .catch(err => {
+        console.log('Error: ', err)
+        setLoading(false)
+      }) 
+  }, [])
+
   return (
-    <div>ShowBook</div>
+    <div className="p-4">
+      <BackButton />
+      <h1 className="text-3xl my-4">Show Book</h1>
+    </div>>
   )
 }
 
